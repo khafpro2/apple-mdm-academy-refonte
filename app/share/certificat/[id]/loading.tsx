cfbdfc85c8226e2e8d52d5405438448556428ab9
khafpro2 <khafpro@gmail.com>
2026-06-11 13:23:56 +0200

    feat: loading page share/certificat

---
+++ app/share/certificat/[id]/loading.tsx
@@ -0,0 +1,3 @@
+export default function ShareLoading() {
+  return <div className="flex min-h-[60vh] items-center justify-center"><div className="h-8 w-8 animate-spin rounded-full border-2 border-accent border-t-transparent" /></div>;
+}
